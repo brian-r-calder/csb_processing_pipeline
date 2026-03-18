a41f0c840e246bd524e2c0ce6f8d3cd1ecaab277
Brian Miles <selimnairb@gmail.com>
2026-03-18 13:45:31 -0400

    Garage: Refine env vars for use in Python

--- scripts/init.sh
+++ scripts/init.sh
@@ -2,7 +2,7 @@
 DOCKER_COMPOSE="docker compose -f ${CONTENT_ROOT}/tests/docker-compose.yml"
 
 AWS_REGION=garage
-ENDPOINT_URL='http://127.0.0.1:3900'
-AWS_CLI="aws --endpoint-url "${ENDPOINT_URL}" --region ${AWS_REGION}"
+AWS_ENDPOINT_URL='http://127.0.0.1:3900'
+AWS_CLI="aws --endpoint-url "${AWS_ENDPOINT_URL}" --region ${AWS_REGION}"
 GARAGE_KEY_FILENAME=.garage-app-key
 BUCKET_NAME='csb-dest'
