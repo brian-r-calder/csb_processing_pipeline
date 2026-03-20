79b6bd294e95e7d6ae0d2084f125165fa8870bc0
Brian Miles <selimnairb@gmail.com>
2026-03-20 09:10:01 -0400

    Use parents list instead of chaining calls to parent

--- src/ocscsb/command/__main__.py
+++ src/ocscsb/command/__main__.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from ocscsb.command import scrape
 
 if __name__ == '__main__':
-    content_root = Path(__file__).parent.parent.parent.parent
+    content_root = Path(__file__).parents[3]
     data_root = content_root / 'data'
     input_shp = data_root / 'tessellation_testing' / 'tessellation_testing.gpkg'
     output_dir = data_root / 'out-dbg'
